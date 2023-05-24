495f2fb1e0977815a844af470bcb4632181d976c
kapinath <134500190+kapinath@users.noreply.github.com>
2023-05-24 23:06:06 +1200

    project 1

---
+++ kapinath.py
@@ -0,0 +1,13 @@
+print ('Hello, world!')
+person=input('what is your name?')
+print('hello',person)
+
+age=14
+print('i am ',age,' years old')
+14
+weight=40
+print('my weight is ',weight,' kg')
+40
+height=138
+print('my height is ',height,' cm')
+138
